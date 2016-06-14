f4e263f2f870dc9ede331e0204a814f02fefdd52
Florian Kunneman <f.kunneman@gmail.com>
2016-06-14 17:18:47 +0200

    updated and documented featurizer class

--- featurize_foliadir.py
+++ featurize_foliadir.py
@@ -10,16 +10,13 @@ outdir = sys.argv[2]
 
 files = os.listdir(foliadir)
 
+#ft = featurizer.Featurizer(lowercase = False, skip_punctuation = False, setname = 'piccl')
+ft = featurizer.Featurizer()
+
 for f in files:
     print(f, foliadir + f)
     doc = folia.Document(file = foliadir + f, encoding = 'utf-8')
-    ft = featurizer.Featurizer(doc)
-    ft.extract_words()
+    features = ft.extract_words(doc)
     outfile = outdir + f[:-4] + '.txt'
     with open(outfile, 'w', encoding = 'utf-8') as f_out:
-        f_out.write(' '.join(ft.features))
-
-
-    
-    
-
+        f_out.write(' '.join(features))
