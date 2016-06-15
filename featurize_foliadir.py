0f7f9b535f4f56ae4721a6010db0ac96fd82df8b
Maarten van Gompel <proycon@anaproy.nl>
2016-06-15 11:06:51 +0200

    moving stuff into function and adding a main, to preserve previous non-LuigiNLP behaviour as well

--- featurize_foliadir.py
+++ featurize_foliadir.py
@@ -5,26 +5,29 @@ import os
 from pynlpl.formats import folia
 import featurizer
 
-foliadir = sys.argv[1]
-outdir = sys.argv[2]
+def featurizer_foliadir(foliadir, outdir):
+    files = os.listdir(foliadir)
 
-files = os.listdir(foliadir)
+    #ft = featurizer.Featurizer(lowercase = False, skip_punctuation = False, setname = 'piccl')
+    ft = featurizer.Featurizer()
 
-#ft = featurizer.Featurizer(lowercase = False, skip_punctuation = False, setname = 'piccl')
-ft = featurizer.Featurizer()
+    for f in files:
+        print(f, foliadir + f)
+        try:
+            outfile = f[:-4] + '.txt'
+            if outfile in os.listdir(outdir):
+                print('file already generated, skipping')
+                continue
+            else:
+                doc = folia.Document(file = foliadir + f, encoding = 'utf-8')
+                features = ft.extract_words(doc)
+                with open(outfile, 'w', encoding = 'utf-8') as f_out:
+                    f_out.write(' '.join(features))
+        except:
+            #exc_type, exc_obj, exc_tb = sys.exc_info()
+            print('Error parsing doc', foliadir + f)
 
-for f in files:
-    print(f, foliadir + f)
-    try:
-        outfile = f[:-4] + '.txt'
-        if outfile in os.listdir(outdir):
-            print('file already generated, skipping')
-            continue
-        else:
-            doc = folia.Document(file = foliadir + f, encoding = 'utf-8')
-            features = ft.extract_words(doc)
-            with open(outfile, 'w', encoding = 'utf-8') as f_out:
-                f_out.write(' '.join(features))
-    except:
-        #exc_type, exc_obj, exc_tb = sys.exc_info()
-        print('Error parsing doc', foliadir + f)
+
+if __name__ == '__main__':
+    foliadir = sys.argv[1]
+    outdir = sys.argv[2]
