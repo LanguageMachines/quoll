17e7b7b8e4cc8f09670cd595a7d4b42b59b9477e
Maarten van Gompel <proycon@anaproy.nl>
2016-06-15 11:16:38 +0200

    added FeaturizerTask_single

--- featurize_foliadir.py
+++ featurize_foliadir.py
@@ -3,6 +3,9 @@ import sys
 import os
 
 from pynlpl.formats import folia
+from luigi import Parameter
+from luiginlp.engine import Task, StandardWorkflowComponent, InputFormat, registercomponent, TargetInfo
+from luiginlp.util import replaceextension
 import featurizer
 
 def featurizer_foliadir(foliadir, outdir):
@@ -26,6 +29,27 @@ def featurizer_foliadir(foliadir, outdir):
         except:
             #exc_type, exc_obj, exc_tb = sys.exc_info()
             print('Error parsing doc', foliadir + f)
+
+class FeaturizerTask_single(Task):
+    """Featurizes a single FoLiA XML file"""
+
+    outputdir = Parameter(default="") #optional output directory (output will be written to same dir as inputfile otherwise)
+
+    in_folia = None #input slot
+
+    def out_featuretxt(self):
+        if self.outputdir and self.outputdir != '.': #handle optional outputdir
+            return TargetInfo(self, os.path.join(self.outputdir, os.path.basename(replaceextension(self.in_folia().path, '.folia.xml','.features.txt'))))
+        else:
+            return TargetInfo(self, replaceextension(self.in_folia().path, '.folia.xml','.features.txt'))
+
+    def run(self):
+        ft = featurizer.Featurizer()
+        doc = folia.Document(file=self.in_folia().path, encoding = 'utf-8')
+        features = ft.extract_words(doc)
+        with open(self.out_featuretxt().path,'w',encoding = 'utf-8') as f_out:
+            f_out.write(' '.join(features))
+
 
 
 if __name__ == '__main__':
