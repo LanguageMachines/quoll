ec1caf7a87e886861e240ed6e5cad22a87050961
Wessel Stoop <wstoop@applejack.science.ru.nl>
2016-06-16 16:27:11 +0200

    Added sklearn vectorizer task

--- text_classification/prepare_instances.py
+++ text_classification/prepare_instances.py
@@ -1,9 +1,15 @@
+#NOTE: not used in production yet, will need tweaking
+
 import os
 import os.path
+
+import numpy
 
 import luiginlp
 from luiginlp.engine import Task,TargetInfo, StandardWorkflowComponent, registercomponent, InputFormat
 from luiginlp.util import replaceextension
+
+from sklearn.feature_extraction.text import CountVectorizer
 
 class FolderStructureToLCSIndex(Task):
     """Assumes you have a folder structures with 1 file for every document, organized in folders per document type"""
@@ -26,6 +32,33 @@ class FolderStructureToLCSIndex(Task):
             for file_name in os.listdir(full_folder_name):
                 indexfile.write(file_name+'\t'+folder_name+'\n')
 
+class FolderStructureToSklearnVector(Task):
+
+    in_txtdir = None
+
+    def out_npy(self):
+        return TargetInfo(self, replaceextension(self.in_txtdir().path, '.txtdir','.npy'))
+
+    def run(self):
+        all_file_names = []
+        all_labels = []
+
+        for n, folder_name in enumerate(os.listdir(self.in_txtdir().path)):
+
+            full_folder_name = self.in_txtdir().path+'/'+folder_name
+
+            if os.path.isfile(full_folder_name):
+                continue
+
+            for file_name in os.listdir(full_folder_name):
+                all_labels.append(n)
+                all_file_names.append(full_folder_name+'/'+file_name)
+
+        vectorizer = CountVectorizer(input='filename')
+        vector = vectorizer.fit_transform(all_file_names)
+        numpy.save(self.out_npy().path,vector)
+        numpy.save('labels',numpy.array(all_labels)) #Where and how do we want to save this?
+
 #This is just to test the tasks above
 if __name__ == '__main__':
 
@@ -34,7 +67,8 @@ if __name__ == '__main__':
         """Processes a whole dir, strategy B"""
 
         def autosetup(self):
-            return (FolderStructureToLCSIndex)
+            #return (FolderStructureToLCSIndex)
+            return (FolderStructureToSklearnVector)
 
         def accepts(self):
             return (InputFormat(self, format_id='txtdir', extension='txtdir',directory=True))
