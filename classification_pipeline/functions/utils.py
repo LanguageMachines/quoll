8129b724527a547bcb4f06da0ee5076178af0e8a
fkunneman <f.kunneman@let.ru.nl>
2016-12-08 13:22:22 +0100

    fix

--- classification_pipeline/functions/utils.py
+++ classification_pipeline/functions/utils.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env 
 
-import xlrd
-import json
-import csv
 import re
 import os
 import datetime
@@ -10,65 +7,6 @@ import functools
 from collections import Counter
 from collections import defaultdict
 import numpy as np
-
-import datahandler
-
-def write_csv(rows, outfile):
-    """
-    CSV writer
-    =====
-    Function to write rows to a file in csv format
-
-    Parameters
-    -----
-    rows : list of lists
-    outfile : str
-        The name of the file to write the rows to
-    """
-    with open(outfile, 'w', encoding = 'utf-8') as csvfile:
-        writer = csv.writer(csvfile)
-        for row in rows:
-            writer.writerow(row)
-
-def read_json(filename):
-    """
-    Reader
-    =====
-    Function to read in a json file
-
-    Parameters
-    -----
-    filename : Name of the json formatted file
-        function presumes json output of twiqs
-
-    Returns
-    -----
-    rows : a list of lists
-        each list corresponds to a row with values (in the right order)
-    """
-
-    month = {"Jan" : "01", "Feb" : "02", "Mar" : "03", "Apr" : "04", 
-        "May" : "05", "Jun" : "06", "Jul" : "07", "Aug" : "08", 
-        "Sep" : "09", "Oct" : "10", "Nov" : "11", "Dec" : "12"}
-    date_time = re.compile(r"(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)" + 
-        r" (\d+) (\d{2}:\d{2}:\d{2}) \+\d+ (\d{4})")
-
-    rows = []
-    with open(filename, encoding = "utf-8") as js:
-        for line in js.readlines():
-            decoded = json.loads(line)
-            if "twinl_lang" in decoded and decoded["twinl_lang"] != "dutch":
-                continue
-            tweet_id = decoded["id"]
-            user_id = decoded["user"]["id"]
-            dtsearch = date_time.search(decoded["created_at"]).groups()
-            date = dtsearch[3] + "-" + month[dtsearch[0]] + "-" + dtsearch[1]
-            time = dtsearch[2]
-            username = decoded["user"]["screen_name"]
-            text = decoded["text"]
-            rows.append(["-"] + [tweet_id, user_id, date, time, username, text] + ["-"])
-    return rows
-
 
 def return_folds(instances, split_user = False, n = 10):
     folds = []
@@ -139,41 +77,6 @@ def format_table(data, widths):
             print('Format table: number of width values and row values do not correspond, using tabs instead')
             output.append('\t'.join(row))
     return output
-
-def bundle_data(docs, outfile):
-    if len(docs) > 1:
-        dh_bundle = datahandler.Datahandler()
-        rows_bundle = []
-        for doc in docs:
-            rows_bundle.extend(doc.rows)
-        dh_bundle.set_rows(rows_bundle)
-    elif len(docs) == 1:
-        dh_bundle = docs[0]
-    dh_bundle.write_csv(outfile)
-    return dh_bundle
-
-def balance_data(dh, outfile):
-    labelsequence = dh.dataset['label']
-    # extract distribution
-    labels = list(set(labelsequence))
-    distribution = []
-    for label in labels:
-        distribution.append((label, labelsequence.count(label)))
-    sorted_distribution = sorted(distribution, key = lambda k : k[1])
-    lowest = sorted_distribution[0][1]
-    # split dataset based on labels and sample labels:
-    handlers = []
-    for i, label_count in enumerate(distribution):
-        label = label_count[0]
-        count = label_count[1]
-        indices = [docindex for docindex in range(len(labelsequence)) if labelsequence[docindex] == label]
-        docs = [dh.rows[docindex] for docindex in indices]
-        handler = datahandler.Datahandler()
-        handler.set_rows(docs)
-        if count > lowest:
-            handler.sample(lowest)
-        handlers.append(handler)
-    return bundle_data(handlers, outfile)
 
 def save_sparse_csr(filename, array):
     np.savez(filename, data = array.data, indices = array.indices,
