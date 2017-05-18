a977b9f370c6b407d71b2b0bed4248821e65e13f
fkunneman <f.kunneman@let.ru.nl>
2017-05-18 10:43:53 +0200

    added procedure to filter features, train a svorim classifier and apply it on unlabeled document vectors

---
+++ quoll/classification_pipeline/modules/train_apply_svorim_filter.py
@@ -0,0 +1,85 @@
+
+from luiginlp.engine import Task, StandardWorkflowComponent, WorkflowComponent, InputFormat, InputComponent, registercomponent, InputSlot, Parameter, BoolParameter, IntParameter
+
+from quoll.classification_pipeline.modules import vectorize_instances, classify_instances, report_performance, filter_features_correlation, rank_features_ordinal_correlation
+
+@registercomponent
+class TrainApplySvorimFilter(WorkflowComponent):
+
+    train = Parameter()
+    trainlabels = Parameter()
+    test = Parameter()
+    featurenames_train = Parameter()
+    featurenames_test = Parameter()
+    documents_test = Parameter()
+
+    threshold_strength = Parameter()
+    threshold_correlation = Parameter()
+    svorim_path = Parameter()
+
+    def accepts(self):
+        return [ ( InputFormat(self,format_id='train',extension='.vectors.npz',inputparameter='train'), InputFormat(self, format_id='trainlabels', extension='.labels', inputparameter='trainlabels'), InputFormat(self, format_id='test', extension='.vectors.npz',inputparameter='test'), InputFormat(self, format_id='testlabels', extension='.labels', inputparameter='testlabels'), InputFormat(self,format_id='documents_test',extension='.txt',inputparameter='documents_test'), InputFormat(self, format_id='featurenames_train', extension='.txt', inputparameter='featurenames_train'), InputFormat(self, format_id='featurenames_test', extension='.txt', inputparameter='featurenames_test') ) ]
+
+    def setup(self, workflow, input_feeds):
+
+        feature_ranker = workflow.new_task('rank_features',rank_features_ordinal_correlation.RankFeaturesOrdinalTask,autopass=True)
+        feature_ranker.in_vectors = input_feeds['train']
+        feature_ranker.in_labels = input_feeds['trainlabels']
+        feature_ranker.in_featurenames = input_feeds['featurenames_train']
+
+        feature_correlation = workflow.new_task('feature_correlation',rank_features_ordinal_correlation.CalculateFeatureCorrelationTask,autopass=True)
+        feature_correlation.in_vectors = input_feeds['train']
+        feature_correlation.in_featurenames = input_feeds['featurenames_train']
+
+        feature_filter = workflow.new_task('filter_features',filter_features_correlation.FilterFeaturesFTask,autopass=True, threshold_strength=self.threshold_strength,threshold_correlation=self.threshold_correlation)
+        feature_filter.in_featurenames = input_feeds['featurenames_train']
+        feature_filter.in_featstrength = feature_ranker.out_ranked_features
+        feature_filter.in_featcorr = feature_correlation.out_feature_correlation
+
+        train_vector_transformer = workflow.new_task('transform_vectors',vectorize_instances.TransformVectorsTask,autopass=True)
+        train_vector_transformer.in_vectors = input_feeds['train']
+        train_vector_transformer.in_selection = feature_filter.out_filtered_features_index
+
+        index_generator = workflow.new_task('generate_feature_indices',GenerateFeatureIndices,autopass=True)
+        index_generator.in_original_featurenames = input_feeds['featurenames_train']
+        index_generator.in_selected_feature_indices = feature_filter.out_filtered_features
+        index_generator.in_new_featurenames = input_feeds['featurenames_test']
+
+        test_vector_transformer = workflow.new_task('transform_test_vectors',vectorize_instances.TransformVectorsTask,autopass=True)
+        test_vector_transformer.in_vectors = input_feeds['test']
+        test_vector_transformer.in_selection = index_generator.out_features_index
+
+        classifier = workflow.new_task('svorim_classifier', classify_instances.SvorimClassifier, autopass=False, svorim_path=self.svorim_path)
+        classifier.in_train = train_vector_transformer.out_vectors
+        classifier.in_labels = input_feeds['trainlabels']
+        classifier.in_test = test_vector_transformer.out_vectors
+
+        reporter = workflow.new_task('report_performance', report_performance.ReportDocpredictions:, autopass=True)
+        reporter.in_predictions = classifier.out_classifications
+        reporter.in_documents = input_feeds['documents_test']
+
+        return reporter
+
+class GenerateFeatureIndices(Task):
+
+    in_all_featurenames = InputSlot()
+    in_selected_featurenames = InputSlot()
+
+    def out_features_index(self):
+        return self.outputfrominput(inputformat='selected_featurenames', stripextension='.txt', addextension='.incices_test.txt')
+
+    def run(self):
+
+        with open(self.in_all_featurenames().path,'r',encoding='utf-8') as infile:
+            all_featurenames = infile.read().strip().split('\n')
+
+        with open(self.in_selected_featurenames().path,'r',encoding='utf-8') as infile:
+            selected_featurenames = infile.read().strip().split('\n')
+
+        indices = [i for i,fn in enumerate(all_featurenames) if fn in selected_featurenames]
+
+        with open(self.out_features_index().path, 'w', encoding='utf-8') as out:
+            out.write(' '.join(indices))
+
+        with open(out_fns,'w',encoding='utf-8') as out:
+            out.write('\n'.join([str(x[1]) for x in indices]))
