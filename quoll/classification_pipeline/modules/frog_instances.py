29bdc07cbbeeadab5fa0cd30e64d7ee3bc3e7508
fkunneman <f.kunneman@let.ru.nl>
2017-06-27 10:02:31 +0200

    fix to frog component

--- quoll/classification_pipeline/modules/frog_instances.py
+++ quoll/classification_pipeline/modules/frog_instances.py
@@ -1,118 +1,108 @@
 
 from luiginlp.engine import Task, StandardWorkflowComponent, InputFormat, InputComponent, registercomponent, InputSlot, Parameter, BoolParameter
 
-import ucto
+import frog
 import glob
 
-class Tokenize_instances(Task):
-    """"Tokenizes a file one document per line"""
+class Frog_instances(Task):
+    """"Frogs a file one document per line"""
     in_txt = InputSlot()
 
-    tokconfig = Parameter()
+    frogconfig = Parameter()
     strip_punctuation = BoolParameter()
 
-    def out_tokenized(self):
-        return self.outputfrominput(inputformat='txt', stripextension='.txt', addextension='.tok.txt')
+    def out_frogged(self):
+        return self.outputfrominput(inputformat='txt', stripextension='.txt', addextension='.frog.json')
 
     def run(self):
 
+        # open file
         with open(self.in_txt().path, 'r', encoding = 'utf-8') as file_in:
             lines = file_in.readlines()
 
-        with open(self.out_tokenized().path,'w',encoding = 'utf-8') as file_out:
-            tokenizer = ucto.Tokenizer(self.tokconfig)
-            for line in lines:
-                tokenizer.process(line)
-                tokens = []
-                for token in tokenizer:
-                    if not (self.strip_punctuation and token.tokentype == 'PUNCTUATION'):
-                        tokens.append(token.text)
-                file_out.write(' '.join(tokens) + '\n')
+        # initialize frogger
+        fo = frog.FrogOptions(ner=False, chunking=False, mwu=False, lemma=True, morph=False, daringmorph=False)
+        frogger = frog.Frog(fo,self.frogconfig)
 
+        # initialize print reports    
+        numlines = len(lines)
+        if numlines > 1000:
+            reports = range(0, numlines, 1000)
+        elif numlines > 100:
+            reports = range(0, numlines, 10)
+        else:
+            reports = range(numlines)
 
-class Tokenize_document(Task):
-    """"Tokenizes a document"""
-    in_txt = InputSlot()
+        # for each line
+        documents = []
+        for i, line in enumerate(lines):
+            if i in reports:
+                print(i, 'of', numlines, 'lines frogged.')
+            # frog
+            frogged = frogger.process(line)
+            # initialize sentences
+            sentences = []
+            sentence = []
+            # add each token to the sentence...
+            for token in frogged:
+                if not (self.strip_punctuation and token.tokentype == 'PUNCTUATION'):
+                    sentence.append({'text':token['text'], 'lemma':token['lemma'], 'pos':token['pos']})
+                # ...until the sentence ends
+                if 'eos' in token:
+                    sentences.append(sentence)
+                    # initialize a new sentence
+                    sentence = []
+            # scrape last bits of info
+            if len(sentence) > 0:
+                sentences.append(sentence)
+            documents.append(sentences)
 
-    tokconfig = Parameter()
-    strip_punctuation = BoolParameter()
+        # write output
+        with open(self.out_frogged().path,'w',encoding = 'utf-8') as file_out:
+            json.dumps(documents)
 
-    def out_tokenized(self):
-        return self.outputfrominput(inputformat='txt', stripextension='.txt', addextension='.tok.txt')
-
-    def run(self):
-
-        with open(self.in_txt().path, 'r', encoding = 'utf-8') as file_in:
-            parts = file_in.read().strip().split('\n')
-
-        with open(self.out_tokenized().path,'w',encoding = 'utf-8') as file_out:
-            tokenizer = ucto.Tokenizer(self.tokconfig)
-            for part in parts:
-                tokenizer.process(part)
-                tokens = []
-                for token in tokenizer:
-                    if not (self.strip_punctuation and token.tokentype == 'PUNCTUATION'):
-                        tokens.append(token.text)
-                    if token.isendofsentence():
-                        file_out.write(' '.join(tokens) + '\n')
-                        tokens = []
-
-class Tokenize_txtdir(Task):
+class Frog_txtdir(Task):
     """"Tokenizes a directory with files"""
 
     in_txtdir = InputSlot()
 
-    tokconfig = Parameter()
+    frogconfig = Parameter()
     strip_punctuation = BoolParameter()
 
-    def out_toktxtdir(self):
-        return self.outputfrominput(inputformat='txtdir', stripextension='.txtdir', addextension='.tok.txtdir')
+    def out_frogjsondir(self):
+        return self.outputfrominput(inputformat='txtdir', stripextension='.txtdir', addextension='.frog.jsondir')
 
     def run(self):
         #Set up the output directory, will create it and tear it down on failure automatically
-        self.setup_output_dir(self.out_toktxtdir().path)
+        self.setup_output_dir(self.out_frogjsondir().path)
 
         #gather input files
         inputfiles = [ filename for filename in glob.glob(self.in_txtdir().path + '/*.txt') ]
 
-        print('Running Tokenizer...')
-        yield [ Tokenize_doc(inputfile=inputfile,outputdir=self.out_toktxtdir().path,tokconfig=self.tokconfig,strip_punctuation=self.strip_punctuation) for inputfile in inputfiles ]
-
+        print('Running Frogger...')
+        yield [ Frog_component(inputfile=inputfile,outputdir=self.out_toktxtdir().path,frogconfig=self.tokconfig,strip_punctuation=self.strip_punctuation) for inputfile in inputfiles ]
 
 
 @registercomponent
-class Tokenize(StandardWorkflowComponent):
+class Frog_component(StandardWorkflowComponent):
 
-    tokconfig = Parameter()
+    frogconfig = Parameter()
     strip_punctuation = BoolParameter()
 
     def autosetup(self):
-        return Tokenize_instances
+        return Frog_instances
 
     def accepts(self):
         return InputFormat(self, format_id='txt', extension='.txt')
 
 @registercomponent
-class Tokenize_doc(StandardWorkflowComponent):
+class Frog_dir(StandardWorkflowComponent):
 
-    tokconfig = Parameter()
+    frogconfig = Parameter()
     strip_punctuation = BoolParameter()
 
     def autosetup(self):
-        return Tokenize_document
-
-    def accepts(self):
-        return InputFormat(self, format_id='txt', extension='.txt')
-
-
-@registercomponent
-class Tokenize_dir(StandardWorkflowComponent):
-
-    tokconfig = Parameter()
-    strip_punctuation = BoolParameter()
-
-    def autosetup(self):
-        return Tokenize_txtdir
+        return Frog_txtdir
 
     def accepts(self):
         return InputFormat(self, format_id='txtdir', extension='.txtdir')
