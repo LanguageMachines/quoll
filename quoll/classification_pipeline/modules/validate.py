01130f4ea38afaaf517e0320713ec2f7ae34bef3
fkunneman <f.kunneman@let.ru.nl>
2018-05-25 08:35:13 +0200

    seperated the MakeBins module

--- quoll/classification_pipeline/modules/validate.py
+++ quoll/classification_pipeline/modules/validate.py
@@ -1,8 +1,6 @@
 
 import numpy
 from scipy import sparse
-import glob
-from collections import defaultdict
 
 from luiginlp.engine import Task, StandardWorkflowComponent, WorkflowComponent, InputFormat, InputComponent, registercomponent, InputSlot, Parameter, BoolParameter, IntParameter, FloatParameter
 
@@ -14,36 +12,6 @@ from quoll.classification_pipeline.functions import reporter, nfold_cv_functions
 #################################################################
 ### Tasks #######################################################
 #################################################################
-
-class MakeBins(Task):
-
-    in_labels = InputSlot()
-
-    n = IntParameter()
-    steps = IntParameter(default = 1)
-    teststart = IntParameter(default=0)
-    testend = IntParameter(default=-1)
-
-    def out_bins(self):
-        return self.outputfrominput(inputformat='labels', stripextension='.labels', addextension='.' + str(self.n) + 'folds.bins.csv')
-        
-    def run(self):
-
-        # open labels
-        with open(self.in_labels().path,'r',encoding='utf-8') as infile:
-            labels = numpy.array(infile.read().strip().split('\n'))
-
-        # select rows per fold based on shape of the features
-        if self.testend == -1:
-            num_instances = len(labels)
-        else:
-            num_instances = self.testend
-        fold_indices = nfold_cv_functions.return_fold_indices(num_instances,self.n,self.steps,self.teststart)        
-        
-        # write indices of bins to file
-        lw = linewriter.Linewriter(fold_indices)
-        lw.write_csv(self.out_bins().path)
-
 
 class Fold(Task):
 
