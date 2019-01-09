78d5d23277c8c7486d0238051197cbc08ed27529
fkunneman <f.kunneman@let.ru.nl>
2019-01-09 11:58:27 +0100

    cut import of ensemble from classifier

--- quoll/classification_pipeline/modules/ensemble.py
+++ quoll/classification_pipeline/modules/ensemble.py
@@ -10,7 +10,8 @@ from collections import defaultdict
 from luiginlp.engine import Task, StandardWorkflowComponent, WorkflowComponent, InputFormat, InputComponent, registercomponent, InputSlot, Parameter, BoolParameter, IntParameter
 
 from quoll.classification_pipeline.modules.validate import Validate
-from quoll.classification_pipeline.modules.vectorize import PredictionsToVectors
+from quoll.classification_pipeline.modules.classify import Classify, Train, Predict, TranslatePredictions
+from quoll.classification_pipeline.modules.vectorize import FeaturizeTask, TransformCsv, PredictionsToVectors
 
 from quoll.classification_pipeline.functions import quoll_helpers, vectorizer
 
@@ -171,6 +172,8 @@ class EnsembleTrain(Task):
     ga_parameters = Parameter()
     classify_parameters = Parameter()
     vectorize_parameters = Parameter()
+    featurize_parameters = Parameter()
+    preprocess_parameters = Parameter()
 
     def out_train(self):
         return self.outputfrominput(inputformat='train', stripextension='.features.npz', addextension='.ensemble.vectors.npz')
@@ -183,7 +186,8 @@ class EnsembleTrain(Task):
         if self.complete(): # necessary as it will not complete otherwise
             return True
 
-        yield Ensemble(train=self.in_train().path,trainlabels=self.in_trainlabels().path,ga_parameters=self.ga_parameters,classify_parameters=self.classify_parameters,vectorize_parameters=self.vectorize_parameters)
+        kwargs = quoll_helpers.decode_task_input(['classify','ga','vectorize'],[self.classify_parameters,self.ga_parameters,self.vectorize_parameters,self.featurize_parameters,self.preprocess_parameters])        
+        yield Ensemble(train=self.in_train().path,trainlabels=self.in_trainlabels().path,**kwargs)
 
 
 class EnsembleTrainTest(Task):
@@ -195,6 +199,8 @@ class EnsembleTrainTest(Task):
     ga_parameters = Parameter()
     classify_parameters = Parameter()
     vectorize_parameters = Parameter()
+    featurize_parameters = Parameter()
+    preprocess_parameters = Parameter()
 
     def out_train(self):
         return self.outputfrominput(inputformat='train', stripextension='.features.npz', addextension='.ensemble.vectors.npz')
@@ -205,12 +211,16 @@ class EnsembleTrainTest(Task):
     def out_test(self):
         return self.outputfrominput(inputformat='test', stripextension='.features.npz', addextension='.ensemble.vectors.npz')
 
+    def out_predictions(self):
+        return self.outputfrominput(inputformat='test', stripextension='.features.npz', addextension='.ensemble.predictions.txt')        
+
     def run(self):
 
         if self.complete(): # necessary as it will not complete otherwise
             return True
-        
-        yield Ensemble(train=self.in_train().path,trainlabels=self.in_trainlabels().path,test=self.in_test().path,ga_parameters=self.ga_parameters,classify_parameters=self.classify_parameters,vectorize_parameters=self.vectorize_parameters)
+
+        kwargs = quoll_helpers.decode_task_input(['classify','ga','vectorize'],[self.classify_parameters,self.ga_parameters,self.vectorize_parameters,self.featurize_parameters,self.preprocess_parameters])        
+        yield Ensemble(train=self.in_train().path,trainlabels=self.in_trainlabels().path,test=self.in_test().path,**kwargs)
 
 
 ##################################################################
@@ -224,35 +234,218 @@ class Ensemble(WorkflowComponent):
     trainlabels = Parameter()
     test = Parameter(default = 'xxx.xxx')
 
-    ga_parameters = Parameter()
-    classify_parameters = Parameter()
-    vectorize_parameters = Parameter()
+    # featureselection parameters
+    ga = BoolParameter()
+    num_iterations = IntParameter(default=300)
+    population_size = IntParameter(default=100)
+    elite = Parameter(default='0.1')
+    crossover_probability = Parameter(default='0.9')
+    mutation_rate = Parameter(default='0.3')
+    tournament_size = IntParameter(default=2)
+    n_crossovers = IntParameter(default=1)
+    stop_condition = IntParameter(default=5)
+    weight_feature_size = Parameter(default='0.0')
+    steps = IntParameter(default=1)
+    sampling = BoolParameter() # repeated resampling to prevent overfitting
+    samplesize = Parameter(default='0.8') # size of trainsample
+    
+    # classifier parameters
+    classifier = Parameter(default='naive_bayes')
+    ensemble = Parameter(default=False)
+    ordinal = BoolParameter()
+    jobs = IntParameter(default=1)
+    iterations = IntParameter(default=10)
+    scoring = Parameter(default='roc_auc') # optimization metric for grid search
+    linear_raw = BoolParameter()
+    scale = BoolParameter()
+    min_scale = Parameter(default='0')
+    max_scale = Parameter(default='1')
+
+    random_clf = Parameter(default='equal')
+    
+    nb_alpha = Parameter(default='1.0')
+    nb_fit_prior = BoolParameter()
+    
+    svm_c = Parameter(default='1.0')
+    svm_kernel = Parameter(default='linear')
+    svm_gamma = Parameter(default='0.1')
+    svm_degree = Parameter(default='1')
+    svm_class_weight = Parameter(default='balanced')
+
+    lr_c = Parameter(default='1.0')
+    lr_solver = Parameter(default='liblinear')
+    lr_dual = BoolParameter()
+    lr_penalty = Parameter(default='l2')
+    lr_multiclass = Parameter(default='ovr')
+    lr_maxiter = Parameter(default='1000')
+
+    linreg_fit_intercept = Parameter(default='1')
+    linreg_normalize = Parameter(default='0')
+    linreg_copy_X = Parameter(default='1')
+
+    xg_booster = Parameter(default='gbtree') # choices: ['gbtree', 'gblinear']
+    xg_silent = Parameter(default='1') # set to '1' to mute printed info on progress
+    xg_learning_rate = Parameter(default='0.1') # choose 'search' for automatic grid search, define grid values manually by giving them divided by space 
+    xg_min_child_weight = Parameter(default='1') # choose 'search' for automatic grid search, define grid values manually by giving them divided by space 
+    xg_max_depth = Parameter(default='6') # choose 'search' for automatic grid search, define grid values manually by giving them divided by space 
+    xg_gamma = Parameter(default='0') # choose 'search' for automatic grid search, define grid values manually by giving them divided by space 
+    xg_max_delta_step = Parameter(default='0')
+    xg_subsample = Parameter(default='1') # choose 'search' for automatic grid search, define grid values manually by giving them divided by space 
+    xg_colsample_bytree = Parameter(default='1.0') # choose 'search' for automatic grid search, define grid values manually by giving them divided by space 
+    xg_reg_lambda = Parameter(default='1')
+    xg_reg_alpha = Parameter(default='0') # choose 'search' for automatic grid search, define grid values manually by giving them divided by space 
+    xg_scale_pos_weight = Parameter('1')
+    xg_objective = Parameter(default='binary:logistic') # choices: ['binary:logistic', 'multi:softmax', 'multi:softprob']
+    xg_seed = Parameter(default='7')
+    xg_n_estimators = Parameter(default='100') # choose 'search' for automatic grid search, define grid values manually by giving them divided by space 
+
+    knn_n_neighbors = Parameter(default='3')
+    knn_weights = Parameter(default='uniform')
+    knn_algorithm = Parameter(default='auto')
+    knn_leaf_size = Parameter(default='30')
+    knn_metric = Parameter(default='euclidean')
+    knn_p = IntParameter(default=2)
+
+    perceptron_alpha = Parameter(default='1.0')
+
+    tree_class_weight = Parameter(default=False)
+
+    # vectorizer parameters
+    weight = Parameter(default = 'frequency') # options: frequency, binary, tfidf
+    prune = IntParameter(default = 5000) # after ranking the topfeatures in the training set, based on frequency or idf weighting
+    delimiter = Parameter(default=',')
+    select = BoolParameter()
+    selector = Parameter(default=False)
+    select_threshold = Parameter(default=False)
+    balance = BoolParameter()
+
+    # featurizer parameters
+    ngrams = Parameter(default='1 2 3')
+    blackfeats = Parameter(default=False)
+    lowercase = BoolParameter()    
+    minimum_token_frequency = IntParameter(default=1)
+    featuretypes = Parameter(default='tokens')
+
+    # ucto / frog parameters
+    tokconfig = Parameter(default=False)
+    frogconfig = Parameter(default=False)
+    strip_punctuation = BoolParameter(default=True)
     
     def accepts(self):
-        return [ ( 
-            InputFormat(self,format_id='train',extension='.features.npz',inputparameter='train'), 
-            InputFormat(self,format_id='trainlabels',extension='.labels',inputparameter='trainlabels'),
-            InputFormat(self,format_id='test',extension='.features.npz',inputparameter='test'), 
-        ) ]
+        return [tuple(x) for x in numpy.array(numpy.meshgrid(*
+            [
+                (
+                InputFormat(self, format_id='modeled_train',extension ='.model.pkl',inputparameter='train'),
+                InputFormat(self, format_id='vectors_train',extension='.vectors.npz',inputparameter='train'),
+                InputFormat(self, format_id='train_csv',extension='.csv',inputparameter='train'),
+                InputFormat(self, format_id='train',extension='.features.npz',inputparameter='train'),
+                InputFormat(self, format_id='train',extension='.tok.txt',inputparameter='train'),
+                InputFormat(self, format_id='train',extension='.tok.txtdir',inputparameter='train'),
+                InputFormat(self, format_id='train',extension='.frog.json',inputparameter='train'),
+                InputFormat(self, format_id='train',extension='.frog.jsondir',inputparameter='train'),
+                InputFormat(self, format_id='train',extension='.txt',inputparameter='train'),
+                InputFormat(self, format_id='train',extension='.txtdir',inputparameter='train')
+                ),
+                (
+                InputFormat(self, format_id='labels_train',extension='.labels',inputparameter='trainlabels')
+                ),
+                (
+                InputFormat(self, format_id='vectors_test',extension='.vectors.npz',inputparameter='test'),
+                InputFormat(self, format_id='test_csv',extension='.csv',inputparameter='test'),
+                InputFormat(self, format_id='test',extension='.features.npz',inputparameter='test'),
+                InputFormat(self, format_id='test',extension='.tok.txt',inputparameter='test'),
+                InputFormat(self, format_id='test',extension='.tok.txtdir',inputparameter='test'),
+                InputFormat(self, format_id='test',extension='.frog.json',inputparameter='test'),
+                InputFormat(self, format_id='test',extension='.frog.jsondir',inputparameter='test'),
+                InputFormat(self, format_id='test',extension='.txt',inputparameter='test'),
+                InputFormat(self, format_id='test',extension='.txtdir',inputparameter='test')
+                ),
+            ]
+            )).T.reshape(-1,3)]
  
     def setup(self, workflow, input_feeds):
 
-        kwargs = quoll_helpers.decode_task_input(['ga','classify','vectorize'],[self.ga_parameters,self.classify_parameters,self.vectorize_parameters])
+        task_args = quoll_helpers.prepare_task_input(['preprocess','featurize','vectorize','classify','ga'],workflow.param_kwargs)
 
-        ensemble_trainer = workflow.new_task('train_ensemble',EnsembleTrainTask,ga_parameters=self.ga_parameters,classify_parameters=self.classify_parameters,vectorize_parameters=self.vectorize_parameters)
-        ensemble_trainer.in_train = input_feeds['train']
-        ensemble_trainer.in_trainlabels = input_feeds['trainlabels']
+        trainlabels = input_feeds['labels_train']
 
-        if 'test' in input_feeds.keys():
+        traininstances = False
+        model = False
 
-            ensemble_predictor = workflow.new_task('predict_ensemble',EnsemblePredictTask,ga_parameters=self.ga_parameters,classify_parameters=self.classify_parameters,vectorize_parameters=self.vectorize_parameters)
-            ensemble_predictor.in_train = input_feeds['train']
-            ensemble_predictor.in_trainlabels = input_feeds['trainlabels']
-            ensemble_predictor.in_test = input_feeds['test']
+        ######################
+        ### featurize ########
+        ######################
 
-            return ensemble_trainer, ensemble_predictor
+        if 'modeled_train' in input_feeds.keys():
+            model = input_feeds['modeled_train']
+        if 'vectors_train' in input_feeds.keys():
+            traininstances = input_feeds['vectors_train']
+        else:
+            elif 'train_csv' in input_feeds.keys():
+                traincsvtransformer = workflow.new_task('train_transformer_csv',TransformCsv,autopass=True,delimiter=self.delimiter)
+                traincsvtransformer.in_csv = input_feeds['featurized_train_csv']
+                trainfeatures = traincsvtransformer.out_features
+            else:
+                trainfeaturizer = workflow.new_task('featurize_train',FeaturizeTask,autopass=False,preprocess_parameters=task_args['preprocess'],featurize_parameters=task_args['featurize'])
+                trainfeaturizer.in_pre_featurized = input_feeds['train']
+                trainfeatures = trainfeaturizer.out_featurized
+
+        if set(['test','test_csv','vectors_test']) & set(list(input_feeds.keys())):
+            
+            if 'vectors_test' in input_feeds.keys():
+                testinstances = input_feeds['vectors_test']
+            elif 'test_csv' in input_feeds.keys():
+                testcsvtransformer = workflow.new_task('test_transformer_csv',TransformCsv,autopass=True,delimiter=self.delimiter)
+                testcsvtransformer.in_csv = input_feeds['featurized_test_csv']
+                testfeatures = testcsvtransformer.out_features
+            else:
+                testfeaturizer = workflow.new_task('featurize_test',FeaturizeTask,autopass=False,preprocess_parameters=task_args['preprocess'],featurize_parameters=task_args['featurize'])
+                testfeaturizer.in_pre_featurized = input_feeds['test']
+                testfeatures = testfeaturizer.out_featurized
+
+        ######################
+        ### Training phase ###
+        ######################
+
+        if set(['test','test_csv','vectors_test']) & set(list(input_feeds.keys())):
+            ensembler = workflow.new_task('ensemble_traintest',EnsembleTrainTest,autopass=True,
+                classify_parameters=task_args['classify'],ga_parameters=task_args['ga'],vectorize_parameters=task_args['vectorize'])
+            ensembler.in_test = testfeatures
+            ensembler.in_train = trainfeatures
+            ensembler.in_trainlabels = trainlabels
+        else: # only train
+            ensembler = workflow.new_task('ensemble_train',EnsembleTrain,autopass=True,
+                classify_parameters=task_args['classify'],ga_parameters=task_args['ga'],vectorize_parameters=task_args['vectorize'])
+            ensembler.in_train = trainfeatures
+            ensembler.in_trainlabels = trainlabels
+
+        trainer = workflow.new_task('train',Train,autopass=True,classifier=self.classifier,classify_parameters=task_args['classify'],ga_parameters=task_args['ga'])
+        trainer.in_train = ensembler.out_train
+        trainer.in_trainlabels = ensembler.out_labels            
+
+        ######################
+        ### Testing phase ####
+        ######################
+
+        if set(['test','test_csv','vectors_test']) & set(list(input_feeds.keys())):
+
+            if not model:
+                model = trainer.out_model
+
+            predictor = workflow.new_task('predictor',Predict,autopass=True,
+                classifier=self.classifier,ordinal=self.ordinal,linear_raw=self.linear_raw,scale=self.scale,ga=self.ga)
+            predictor.in_test = ensembler.out_test
+            predictor.in_trainlabels = ensembler.out_labels
+            predictor.in_model = model
+
+            if self.linear_raw:
+                translator = workflow.new_task('predictor',TranslatePredictions,autopass=True)
+                translator.in_linear_labels = trainlabels
+                translator.in_predictions = predictor.out_predictions
+                return translator
+
+            else:
+                return predictor
 
         else:
-
-            return ensemble_trainer
-
+            return trainer
